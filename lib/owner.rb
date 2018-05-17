079444c8aabb55906c0030ea4a2e73f4e88b3e16
Grace Shih <vocalise@gmail.com>
2018-05-17 03:01:28 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -64,11 +64,11 @@ class Owner
   end
   
   def sell_pets
-     nervous_pets = @pets.map do |animals, animalObjects|
+     @pets.map do |animals, animalObjects|
       animalObjects.each do |animal|
         animal.mood = "nervous"
       end
     end
-    nervous_pets
+    @pets
   end
 end
