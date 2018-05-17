9de6dbc67bdc96ff9de0312589eacab4f4afe9a2
Grace Shih <vocalise@gmail.com>
2018-05-17 02:59:45 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -64,8 +64,7 @@ class Owner
   end
   
   def sell_pets
-    binding.pry
-    @pets.each do |animals, animalObjects|
+    @pets.map do |animals, animalObjects|
       animalObjects.each do |animal|
         animal.mood = "nervous"
       end
